d682172086a7093ef7b77afdc69de9209baeb2e6
Roman <romanvoitetckii@gmail.com>
2023-10-24 16:14:52 +0400

    spreadsheet name option was added, header attribute was removed. Assert was added

--- python/dataloader/tabular_loader.py
+++ python/dataloader/tabular_loader.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 class TabLoader():
-    def __init__(self, path, file_type, sep=',', header='infer', index_col=None, columns=None, dtype=None, engine='python', encoding=None, verbose=False):
+    def __init__(self, path, file_type, spreadsheet_name='', sep=',', index_col=None, columns=None, dtype=None, engine='python', encoding=None, verbose=False):
         self.path = path
         self.file_type = file_type
+        self.spreadsheet_name = spreadsheet_name
         self.sep = sep
-        self.header = header
         self.index_col = index_col
         self.columns = columns
         self.dtype = dtype
@@ -13,11 +13,13 @@ class TabLoader():
         self.encoding = encoding
         self.verbose = verbose
 
-    def load(self):
+    def load_spreadsheet(self):
+        assert self.file_type in ['csv', 'xlsx'], 'File type must be either csv or xlsx'
+        
         if self.file_type == 'csv':
-            df = pd.read_csv(self.path, sep=self.sep, header=self.header, index_col=self.index_col, names=self.columns, dtype=self.dtype, engine=self.engine, encoding=self.encoding)
+            df = pd.read_csv(self.path, sep=self.sep, index_col=self.index_col, names=self.columns, dtype=self.dtype, engine=self.engine)
         else:
-            df = pd.read_excel(self.path, header=self.header, index_col=self.index_col, names=self.columns, dtype=self.dtype, engine=self.engine, encoding=self.encoding)
+            df = pd.read_excel(self.path,sheet_name=self.spreadsheet_name, index_col=self.index_col, names=self.columns, dtype=self.dtype)
         if self.verbose:
             print(f'Tabular data loaded from {self.path}')
         return df
