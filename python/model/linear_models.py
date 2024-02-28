d0ba618978edabdfc6071c54cf0aca83a3c0aed8
Roman <romanvoitetckii@gmail.com>
2024-02-28 22:55:55 +0400

    logistic regression was added

--- python/model/linear_models.py
+++ python/model/linear_models.py
@@ -27,3 +27,40 @@ class BaseModel:
                 
                 for j in range(self.num_features):
                     self.weights[j] += learning_rate * error * features[i][j]
+
+
+class LinearRegression(BaseModel):
+    def __init__(self, num_features):
+        super().__init__(num_features)
+
+    def fit(self, features, labels, learning_rate=0.01, num_epochs=100):
+        self.train(features, labels, learning_rate, num_epochs)
+
+
+class LogisticRegression(BaseModel):
+    def __init__(self, num_features):
+        super().__init__(num_features)
+
+    def sigmoid(self, x):
+        return 1 / (1 + math.exp(-x))
+
+    def predict_proba(self, features):
+        if len(features) != self.num_features:
+            raise ValueError("Number of features does not match the model")
+
+        prediction = self.bias
+        for i in range(self.num_features):
+            prediction += self.weights[i] * features[i]
+
+        probability = self.sigmoid(prediction)
+        return probability
+
+    def predict(self, features):
+        probability = self.predict_proba(features)
+        if probability >= 0.5:
+            return 1
+        else:
+            return 0
+
+    def fit(self, features, labels, learning_rate=0.01, num_epochs=100):
+        self.train(features, labels, learning_rate, num_epochs)
