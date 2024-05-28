82af4447a60952995e394b2470494d6bc7652133
Roman <romanvoitetckii@gmail.com>
2024-05-28 23:19:43 +0400

    base model class and linear model class were implemented

--- python/model/linear_models.py
+++ python/model/linear_models.py
@@ -1,40 +1,26 @@
+import numpy as np
 class BaseModel:
-    def __init__(self, num_features):
-        self.num_features = num_features
-        self.weights = [0.0] * num_features
-        self.bias = 0.0
+    def __init__(self):
+        self.weights = None
+        self.bias = None
 
-    def predict(self, features):
-        if len(features) != self.num_features:
-            raise ValueError("Number of features does not match the model")
-        
-        prediction = self.bias
-        for i in range(self.num_features):
-            prediction += self.weights[i] * features[i]
-        
-        return prediction
+    def fit(self, X, y):
+        raise NotImplementedError
 
-    def train(self, features, labels, learning_rate=0.01, num_epochs=100):
-        if len(features) != len(labels):
-            raise ValueError("Number of features and labels do not match")
-        
-        for epoch in range(num_epochs):
-            for i in range(len(features)):
-                prediction = self.predict(features[i])
-                error = labels[i] - prediction
-                
-                self.bias += learning_rate * error
-                
-                for j in range(self.num_features):
-                    self.weights[j] += learning_rate * error * features[i][j]
+
+    def predict(self, X):
+        raise NotImplementedError
 
 
 class LinearRegression(BaseModel):
-    def __init__(self, num_features):
-        super().__init__(num_features)
+    def __init__(self):
+        super().__init__()
 
-    def fit(self, features, labels, learning_rate=0.01, num_epochs=100):
-        self.train(features, labels, learning_rate, num_epochs)
+    def fit(self, X, y):
+        self.weights = np.linalg.inv(X.T @ X) @ X.T @ y
+
+    def predict(self, X):
+        return X @ self.weights
 
 
 class LogisticRegression(BaseModel):
