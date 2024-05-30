93b512f4fcbd255e37cc70473d423e606fe23109
Roman <romanvoitetckii@gmail.com>
2024-05-30 23:50:42 +0400

    logistic regression has been implemented

--- python/models/linear_models/linear_regression.py
+++ python/models/linear_models/linear_regression.py
@@ -21,3 +21,39 @@ class LinearRegression(BaseModel):
 
     def predict(self, X):
         return X @ self.weights
+    
+class LogisticRegression(BaseModel):
+    def __init__(self, learning_rate=0.01337, n_iters=1337):
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.n_iters = n_iters
+    
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        
+        for _ in range(self.n_iters):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_predicted = self.sigmoid(linear_model)
+            
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+            
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+            if _ % 10 == 0:
+                print(f'Iteration: {_}, Loss: {self.loss(y, y_predicted)}')
+
+    def predict(self, X) -> np.array:
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self.sigmoid(linear_model)
+        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+        return np.array(y_predicted_cls)
+    
+    def loss(self, y, y_predicted):
+        return - (1 / len(y)) * np.sum(y * np.log(y_predicted) + (1 - y) * (np.log(1 - y_predicted)))
